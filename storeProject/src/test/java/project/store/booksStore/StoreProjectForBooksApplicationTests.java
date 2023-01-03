729610e0dbca5a1aa97406f139bc223be87681ca
Mohamed Riabi <mhdriabi2013@gmail.com>
2023-01-03 17:31:38 +0100

    first commit create a spring boot project and fixing some configuration inside the project(application.properties and gitignore)

---
+++ storeProject/src/test/java/project/store/booksStore/StoreProjectForBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package project.store.booksStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProjectForBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
