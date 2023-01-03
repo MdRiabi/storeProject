000c1da4494d88310dc4e588d65dcc87c8f36c86
Mohamed Riabi <mhdriabi2013@gmail.com>
2023-01-03 23:59:11 +0100

    adding @RepositoryRestResource

--- storeProject/src/main/java/project/store/booksStore/entity/Book.java
+++ storeProject/src/main/java/project/store/booksStore/entity/Book.java
@@ -26,16 +26,22 @@ public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id ;
+	
+	
 	private String sku;
 	private String name ;
 	private String description;
+	
 	
 	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
 	
 	@Column(name = "image_url")
 	private String imageUrl;
+	
 	private boolean active;
+	
+	
 	@Column(name = "units_in_stock")
 	private int unitsInStock;
 	
