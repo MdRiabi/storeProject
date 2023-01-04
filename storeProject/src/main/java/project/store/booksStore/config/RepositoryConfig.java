2184b8d13e12f41addc2ee7cdd925f3958a70cd8
Mohamed Riabi <mhdriabi2013@gmail.com>
2023-01-04 22:58:50 +0100

    refactoring the config file by adding entity manager

--- storeProject/src/main/java/project/store/booksStore/config/RepositoryConfig.java
+++ storeProject/src/main/java/project/store/booksStore/config/RepositoryConfig.java
@@ -1,5 +1,9 @@
 package project.store.booksStore.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,9 +14,18 @@ import project.store.booksStore.entity.Book;
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
 	
+	@Autowired
+	private EntityManager entityManager;
+	
 	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-		config.exposeIdsFor(Book.class);
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config , CorsRegistry cors) {
+
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType)
+				.toArray(Class[]::new));
+		
+	
 	}
 
 }
+
