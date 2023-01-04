bc63de1bcbf3288792436e565741a69ec532c1f5
Mohamed Riabi <mhdriabi2013@gmail.com>
2023-01-04 23:49:45 +0100

    create the book type in the app component ts

--- angular-storeProject/src/app/app.component.ts
+++ angular-storeProject/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Book } from './common/book';
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-storeProject';
+books: Book[]= [
+  {
+    sku: "text100",
+    name:"C Programming Language",
+    description: "Learn C programming languange form skratch",
+    unitPrice: 600,
+    imageUrl: "assets/images/books/text-100.png",
+    active: true,
+    unitsInStock: 100,
+    createOn: new Date(),
+    updateOn: null,
+  }
+]
 }
